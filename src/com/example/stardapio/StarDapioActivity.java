c5040281ad3f1281b57ed9ea484d418f8ba1a758
vinids86 <viniciusdsaraiva@gmail.com>
2013-06-11 01:50:12 -0300

    MarkerMap

--- src/com/example/stardapio/StarDapioActivity.java
+++ src/com/example/stardapio/StarDapioActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -21,7 +22,7 @@ public class StarDapioActivity extends FragmentActivity {
 
 	private GoogleMap mMap = null;
 	private GoogleMapOptions options = null;
-	
+
 	private HashMap<Integer, Marker> markerMap = new HashMap<Integer, Marker>();
 
 	@Override
@@ -36,12 +37,30 @@ public class StarDapioActivity extends FragmentActivity {
 				.rotateGesturesEnabled(false).tiltGesturesEnabled(false);
 		SupportMapFragment.newInstance(options);
 
-		Marker marker1 = mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title(
-				"Cara de Fome"));
+		Marker marker1 = mMap.addMarker(new MarkerOptions().position(
+				new LatLng(0, 0)).title("Cara de Fome"));
 		this.markerMap.put(1, marker1);
 
-		mMap.setInfoWindowAdapter(new InfoWindowAdapter() {
+		/*
+		 * while(restaurantes.next()) {
+		 *  
+		 * Marker marker = mMap.addMarker(new
+		 * MarkerOptions().position(new LatLng(restaurantes.getLat(),
+		 * restaurantes.getLng()).title(restaraurantes.getName());
+		 * this.markerMap.put(restaurantes.getId(), marker);
+		 * }
+		 */
+		mMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
 			
+			@Override
+			public void onInfoWindowClick(Marker marker) {
+				// Exibe informacoes do restaurante like, marker.getId()
+				// startActivity Cardapio (y)
+				
+			}
+		});
+		mMap.setInfoWindowAdapter(new InfoWindowAdapter() {
+
 			@Override
 			public View getInfoContents(Marker marker) {
 				TextView view = ((TextView) findViewById(R.id.info));
@@ -55,9 +74,9 @@ public class StarDapioActivity extends FragmentActivity {
 				return null;
 			}
 		});
-		
+
 		mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
-			
+
 			@Override
 			public boolean onMarkerClick(Marker marker) {
 				goCardapio(marker.getId());
@@ -78,7 +97,7 @@ public class StarDapioActivity extends FragmentActivity {
 			throw new RuntimeException("AHahahha");
 		}
 	}
-	
+
 	private void goCardapio(String id) {
 		Intent intent = new Intent(this, CardapioActivity.class);
 		intent.putExtra("idRestaurante", id);
@@ -87,107 +106,71 @@ public class StarDapioActivity extends FragmentActivity {
 }
 
 /*
- *
- * package com.example.stardapio;
-
-import java.io.File;
-import java.util.List;
-
-import android.app.ListActivity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.example.stardapio.adapter.RestaurantAdapter;
-import com.example.stardapio.bean.Restaurant;
-import com.example.stardapio.webservice.RestaurantREST;
-import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.utils.StorageUtils;
-
-public class StarDapioActivity extends ListActivity {
-	ImageLoaderConfiguration config;
-
-	private class GetAsync extends AsyncTask<Void, Void, List<Restaurant>> {
-
-		private ProgressDialog dialog;
-
-		@Override
-		protected void onPreExecute() {
-			dialog = new ProgressDialog(StarDapioActivity.this);
-			dialog.show();
-		}
-
-		@Override
-		protected List<Restaurant> doInBackground(Void... arg0) {
-			RestaurantREST rest = new RestaurantREST();
-			List<Restaurant> restaurantes = null;
-			try {
-				restaurantes = rest.getListaRestaurante();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return restaurantes;
-		}
-
-		@Override
-		protected void onPostExecute(List<Restaurant> result) {
-			ListView lv = getListView();
-
-			RestaurantAdapter adapter = new RestaurantAdapter(
-					getApplicationContext(), result, config);
-
-			lv.setAdapter(adapter);
-			dialog.dismiss();
-		}
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-
-		File cacheDir = StorageUtils.getCacheDirectory(getApplicationContext());
-		config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-				.discCache(new UnlimitedDiscCache(cacheDir)).build();
-		MyApp appConfig = ((MyApp) getApplicationContext());
-		appConfig.setGlobalConfig(config);
-		new GetAsync().execute();
-	}
-
-	@Override
-	protected void onListItemClick(ListView l, View view, int position, long id) {
-		// Intent intent = new Intent(this, CardapioActivity.class);
-		Intent intent = new Intent(this, MenuSlideActivity.class);
-		String selection = l.getItemAtPosition(position).toString();
-		intent.putExtra("nomeDoRestaurante", selection);
-		// Apenas para depuracao
-		String idRestaurante = String.valueOf(id);
-		Toast.makeText(this, idRestaurante, Toast.LENGTH_LONG).show();
-		intent.putExtra("idRestaurante", idRestaurante);
-		startActivity(intent);
-		// startActivity(new Intent(StarDapioActivity.this,
-		// MenuSlideActivity.class));
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	public void startScan(View view) {
-		Intent intent = new Intent(this, QRCodeActivity.class);
-		startActivity(intent);
-	}
-
-}
  * 
+ * package com.example.stardapio;
+ * 
+ * import java.io.File; import java.util.List;
+ * 
+ * import android.app.ListActivity; import android.app.ProgressDialog; import
+ * android.content.Intent; import android.os.AsyncTask; import
+ * android.os.Bundle; import android.view.Menu; import android.view.View; import
+ * android.widget.ListView; import android.widget.Toast;
+ * 
+ * import com.example.stardapio.adapter.RestaurantAdapter; import
+ * com.example.stardapio.bean.Restaurant; import
+ * com.example.stardapio.webservice.RestaurantREST; import
+ * com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache; import
+ * com.nostra13.universalimageloader.core.ImageLo)aderConfiguration; import
+ * com.nostra13.universalimageloader.utils.StorageUtils;
+ * 
+ * public class StarDapioActivity extends ListActivity {
+ * ImageLoaderConfiguration config;
+ * 
+ * private class GetAsync extends AsyncTask<Void, Void, List<Restaurant>> {
+ * 
+ * private ProgressDialog dialog;
+ * 
+ * @Override protected void onPreExecute() { dialog = new
+ * ProgressDialog(StarDapioActivity.this); dialog.show(); }
+ * 
+ * @Override protected List<Restaurant> doInBackground(Void... arg0) {
+ * RestaurantREST rest = new RestaurantREST(); List<Restaurant> restaurantes =
+ * null; try { restaurantes = rest.getListaRestaurante(); } catch (Exception e)
+ * { e.printStackTrace(); } return restaurantes; }
+ * 
+ * @Override protected void onPostExecute(List<Restaurant> result) { ListView lv
+ * = getListView();
+ * 
+ * RestaurantAdapter adapter = new RestaurantAdapter( getApplicationContext(),
+ * result, config);
+ * 
+ * lv.setAdapter(adapter); dialog.dismiss(); } }
+ * 
+ * @Override protected void onCreate(Bundle savedInstanceState) {
+ * super.onCreate(savedInstanceState); setContentView(R.layout.main);
+ * 
+ * File cacheDir = StorageUtils.getCacheDirectory(getApplicationContext());
+ * config = new ImageLoaderConfiguration.Builder(getApplicationContext())
+ * .discCache(new UnlimitedDiscCache(cacheDir)).build(); MyApp appConfig =
+ * ((MyApp) getApplicationContext()); appConfig.setGlobalConfig(config); new
+ * GetAsync().execute(); }
+ * 
+ * @Override protected void onListItemClick(ListView l, View view, int position,
+ * long id) { // Intent intent = new Intent(this, CardapioActivity.class);
+ * Intent intent = new Intent(this, MenuSlideActivity.class); String selection =
+ * l.getItemAtPosition(position).toString();
+ * intent.putExtra("nomeDoRestaurante", selection); // Apenas para depuracao
+ * String idRestaurante = String.valueOf(id); Toast.makeText(this,
+ * idRestaurante, Toast.LENGTH_LONG).show(); intent.putExtra("idRestaurante",
+ * idRestaurante); startActivity(intent); // startActivity(new
+ * Intent(StarDapioActivity.this, // MenuSlideActivity.class)); }
+ * 
+ * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
+ * menu; this adds items to the action bar if it is present.
+ * getMenuInflater().inflate(R.menu.main, menu); return true; }
+ * 
+ * public void startScan(View view) { Intent intent = new Intent(this,
+ * QRCodeActivity.class); startActivity(intent); }
+ * 
+ * }
  */
- 
